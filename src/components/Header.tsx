6b07e071d10bb25e0c64fc9bce37eb2a912492b7
Tiaanz <jwtian126@gmail.com>
2023-07-29 15:54:12 +1200

    added open mini-cart feature

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,28 @@
+import { Dispatch, SetStateAction} from 'react'
 import CartItem from './CartItem'
 
-const Header = () => {
+interface Props{
+  showMiniCart: boolean
+  setShowMiniCart:Dispatch<SetStateAction<boolean>>
+}
+
+const Header = ({showMiniCart,setShowMiniCart}:Props) => {
+
+
   return (
     <div className="header">
-      <div className="cart">My Cart (4)</div>
-      <div className="cart-on-click">My Cart (4)</div>
-      <div className="mini-cart">
-        <CartItem />
-        <CartItem />
+      <div className="cart" onClick={() => setShowMiniCart(true)}>
+        My Cart (4)
       </div>
+      {showMiniCart && (
+        <>
+          <div className="cart-on-click" onClick={()=>setShowMiniCart(false)}>My Cart (4)</div>
+          <div className="mini-cart">
+            <CartItem />
+            <CartItem />
+          </div>
+        </>
+      )}
     </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,46 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import SizeButton from '@/components/SizeButton'
-
-const inter = Inter({ subsets: ['latin'] })
+import Header from '@/components/Header'
+import { useState } from 'react'
 
 export default function Home() {
+
+  const [showMiniCart, setShowMiniCart] = useState(false)
+
   return (
-    <main className="container">
-      <div className="product-image">
-        <Image
-          priority
-          src="/classic-tee.jpg"
-          alt="classic-tee"
-          width={430}
-          height={500}
-          quality={100}
-        ></Image>
-      </div>
-      <div className="product-detail">
-        <h1>Classic Tee</h1>
-        <div className="price">$75.00</div>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco, veniam, quis nostrud
-          exercitation ullamco.
-        </p>
-        <div className="size">
-          SIZE<span className="star">*</span>
+    <>
+      <Header showMiniCart={showMiniCart} setShowMiniCart={setShowMiniCart} />
+      <main className="container" onClick={()=>setShowMiniCart(false)}>
+        <div className="product-image">
+          <Image
+            priority
+            src="/classic-tee.jpg"
+            alt="classic-tee"
+            width={430}
+            height={500}
+            quality={100}
+          ></Image>
         </div>
-        <div className='size-buttons'>
-          <SizeButton size={'S'} />
-          <SizeButton size={'M'} />
-          <SizeButton size={'L'} />
+        <div className="product-detail">
+          <h1>Classic Tee</h1>
+          <div className="price">$75.00</div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco, veniam, quis
+            nostrud exercitation ullamco.
+          </p>
+          <div className="size">
+            SIZE<span className="star">*</span>
+          </div>
+          <div className="size-buttons">
+            <SizeButton size={'S'} />
+            <SizeButton size={'M'} />
+            <SizeButton size={'L'} />
+          </div>
+          <button className="add-to-cart-btn">ADD TO CART</button>
         </div>
-        <button className='add-to-cart-btn'>ADD TO CART</button>
-      </div>
-    </main>
+      </main>
+    </>
   )
 }
