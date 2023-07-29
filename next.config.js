255a337646dde02aac711648f9643ef590f4d9d9
Tiaanz <jwtian126@gmail.com>
2023-07-29 16:14:46 +1200

    added function to fetch data from external API

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images:{domains:['mrdevelopertestassets.s3.ap-southeast-2.amazonaws.com']}
 }
 
 module.exports = nextConfig
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,61 @@
 import Image from 'next/image'
 import SizeButton from '@/components/SizeButton'
 import Header from '@/components/Header'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
+interface ProductProps {
+  id: number
+  title: string
+  description: string
+  price: number
+  imageURL: string
+  sizeOptions: sizeOption[]
+}
+
+interface sizeOption {
+  id: number
+  label: string
+}
 
 export default function Home() {
-
   const [showMiniCart, setShowMiniCart] = useState(false)
+  const [productDetail, setProductDetail] = useState<ProductProps>({
+    id: 0,
+    title: '',
+    description: '',
+    price: 0,
+    imageURL: '',
+    sizeOptions: [],
+  })
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        'https://3sb655pz3a.execute-api.ap-southeast-2.amazonaws.com/live/product'
+      )
+      const productData = (await response.json()) as ProductProps
+      const { id, title, description, price, imageURL, sizeOptions } =
+        productData
+      setProductDetail({
+        id,
+        title,
+        description,
+        price,
+        imageURL,
+        sizeOptions,
+      })
+    }
+    fetchData()
+  }, [])
 
   return (
     <>
       <Header showMiniCart={showMiniCart} setShowMiniCart={setShowMiniCart} />
-      <main className="container" onClick={()=>setShowMiniCart(false)}>
+      <main className="container" onClick={() => setShowMiniCart(false)}>
         <div className="product-image">
           <Image
             priority
-            src="/classic-tee.jpg"
+            src={productDetail.imageURL}
             alt="classic-tee"
             width={430}
             height={500}
@@ -22,21 +63,16 @@ export default function Home() {
           ></Image>
         </div>
         <div className="product-detail">
-          <h1>Classic Tee</h1>
-          <div className="price">$75.00</div>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco, veniam, quis
-            nostrud exercitation ullamco.
-          </p>
+          <h1>{productDetail.title}</h1>
+          <div className="price">${productDetail.price}.00</div>
+          <p>{productDetail.description}</p>
           <div className="size">
             SIZE<span className="star">*</span>
           </div>
           <div className="size-buttons">
-            <SizeButton size={'S'} />
-            <SizeButton size={'M'} />
-            <SizeButton size={'L'} />
+            {productDetail.sizeOptions.map((item) => (
+              <SizeButton key={item.id} size={item.label} />
+            ))}
           </div>
           <button className="add-to-cart-btn">ADD TO CART</button>
         </div>
